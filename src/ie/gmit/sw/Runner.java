dff427f29efa8c549a02d50b4fba0a10cec4202b
Will Hogan <willhogan11@hotmail.com>
2015-11-11 00:01:35 +0000

    Completed SJF

--- src/ie/gmit/sw/Runner.java
+++ src/ie/gmit/sw/Runner.java
@@ -1,4 +1,5 @@
 package ie.gmit.sw;
+import java.util.Arrays;
 import java.util.Scanner;
 
 
@@ -10,7 +11,7 @@ public class Runner{
 		Scanner input = new Scanner(System.in);
 		System.out.println("Please enter the number of processes: ");
 		
-		// Variable / Array Declarations
+		// Variable / Array Declarations used by all methods
 		int sum = 0;
 		double sumWaitTimes = 0;
 		double averageWaitTime = 0;
@@ -25,7 +26,30 @@ public class Runner{
 			processTimes[i] = input.nextInt();
 		}
 		
+		// Choose the Process Scheduling Algorithm
+		System.out.println("Please enter a Process Scheduling Algorithm:\n1 for FCFS\n2 for SJF\n3 for Round Robin");
 		
+		int choice = input.nextInt();
+		switch(choice){
+			case 1:
+				firstComeFirstServed(waitTime, processTimes, sum, sumWaitTimes, averageWaitTime);
+				break;
+			case 2:
+				shortestJobFirst(waitTime, processTimes, sum, sumWaitTimes, averageWaitTime);
+				break;
+			case 3:
+				System.out.println("Round Robin");
+				break;
+			default:
+				System.out.println("Please Enter a valid selection");
+				break;
+		}
+		
+		input.close();
+	}
+	
+	
+	public static void firstComeFirstServed(int waitTime[], int processTimes[], int sum, double sumWaitTimes, double averageWaitTime){
 		// Display list of each process and associated burst times
 		System.out.println("Process Array:");
 		for(int x: processTimes)
@@ -37,38 +61,46 @@ public class Runner{
 			sum = sum + processTimes[i];
 			sumWaitTimes = sumWaitTimes + waitTime[i];
 		}
-		
-		
-		System.out.println("\nWait time array:");
+
 		// Displays values for each processes waitTimes
+		System.out.println("\nWait time array:");
 		for(int i: waitTime){
 			System.out.print("|" + i);
 		}
 		
+		// Calculates the Average wait time for total processes
 		averageWaitTime = sumWaitTimes / waitTime.length;
 		System.out.println("\n\nTotal average Wait ime value is: " + averageWaitTime);
+		
+	} // End firstComeFirstServed
 	
+	
+	
+	public static void shortestJobFirst(int waitTime[], int processTimes[], int sum, double sumWaitTimes, double averageWaitTime){
+		// Display list of each process and associated burst times
+		System.out.println("Process Array:");
+		Arrays.sort(processTimes); // Sorts the array for Shortest Job First
+		for(int x: processTimes)
+			System.out.print("|" + x);
 		
+		// Calculates the Wait time for each process and also calculates the sum of the waitTime[] Array
+		for(int i = 0; i < processTimes.length; i++){
+			waitTime[i] = sum;
+			sum = sum + processTimes[i];
+			sumWaitTimes = sumWaitTimes + waitTime[i];
+		}
+
+		// Displays values for each processes waitTimes
+		System.out.println("\nWait time array:");
+		for(int i: waitTime){
+			System.out.print("|" + i);
+		}
 		
+		// Calculates the Average wait time for total processes
+		averageWaitTime = sumWaitTimes / waitTime.length;
+		System.out.println("\n\nTotal average Wait ime value is: " + averageWaitTime);
 		
-/*		System.out.println("Please enter:\n1 for FCFS\n2 for SJF\n3 for Round Robin");
-		
-		int choice = input.nextInt();
-		switch(choice){
-			case 1:
-				System.out.println("FCFS");
-				break;
-			case 2:
-				System.out.println("SJF");
-				break;
-			case 3:
-				System.out.println("Round Robin");
-				break;
-			default:
-				System.out.println("Please Enter a valid selection");
-				break;
-		}*/
-		
-		input.close();
-	}
-}
+	} // End shortestJobFirst
+	
+} // End class
+
